87579799ddd17e7e76aa6740b804fc599eb485d4
GMS-CYOUNG <GMS-CYOUNG@GMS-CYOUNG-PC>
2016-12-13 16:05:13 +0900

    1. 최초 업로드

---
+++ moon/src/main/java/moon/sample/dao/SampleDAO.java
@@ -0,0 +1,47 @@
+package moon.sample.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import moon.common.dao.AbstractDAO;
+
+@Service("sampleDAO")
+public class SampleDAO extends AbstractDAO{
+
+	@SuppressWarnings("unchecked")
+	public List<Map<String, Object>> selectBoardList(Map<String, Object> map) throws Exception{
+		return (List<Map<String, Object>>)selectList("sample.selectBoardList", map);
+	}
+ 
+	public void updateHitCnt(Map<String, Object> map) throws Exception{
+	    update("sample.updateHitCnt", map);
+	}
+	
+	public void insertBoard(Map<String, Object> map) throws Exception{
+		update("sample.insertBoard", map);
+	}
+	 
+	@SuppressWarnings("unchecked")
+	public Map<String, Object> selectBoardDetail(Map<String, Object> map) throws Exception{
+	    return (Map<String, Object>) selectOne("sample.selectBoardDetail", map);
+	}
+	
+	public void updateBoard(Map<String, Object> map) throws Exception{
+	    update("sample.updateBoard", map);
+	}
+	
+	public void deleteBoard(Map<String, Object> map) throws Exception{
+	    update("sample.deleteBoard", map);
+	}
+	
+	public void insertFile(Map<String, Object> map) throws Exception{
+	    insert("sample.insertFile", map);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Map<String, Object>> selectFileList(Map<String, Object> map) throws Exception{
+	    return (List<Map<String, Object>>)selectList("sample.selectFileList", map);
+	}
+}
---
+++ moon/src/main/java/moon/sample/service/impl/SampleServiceImpl.java
@@ -0,0 +1,78 @@
+package moon.sample.service.impl;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+import moon.sample.dao.SampleDAO;
+import moon.sample.service.SampleService;
+import moon.util.FileUtils;
+
+@Service("sampleService")
+public class SampleServiceImpl implements SampleService {
+	Logger log = Logger.getLogger(this.getClass());
+	
+	@Resource(name="fileUtils")
+    private FileUtils fileUtils;
+	
+	@Resource(name="sampleDAO")
+	private SampleDAO sampleDAO;
+	
+	public List<Map<String, Object>> selectBoardList(Map<String, Object> map) throws Exception {
+		return sampleDAO.selectBoardList(map);
+	}
+
+	public void insertBoard(Map<String, Object> map, HttpServletRequest request) throws Exception {
+		sampleDAO.insertBoard(map);
+		
+		List<Map<String,Object>> list = fileUtils.parseInsertFileInfo(map, request);
+        for(int i=0, size=list.size(); i<size; i++){
+            sampleDAO.insertFile(list.get(i));
+        }
+        /*        
+		MultipartHttpServletRequest multipartHttpServletRequest = (MultipartHttpServletRequest)request;
+	    Iterator<String> iterator = multipartHttpServletRequest.getFileNames();
+	    MultipartFile multipartFile = null;
+	    while(iterator.hasNext()){
+	        multipartFile = multipartHttpServletRequest.getFile(iterator.next());
+	        if(multipartFile.isEmpty() == false){
+	            log.debug("------------- file start -------------");
+	            log.debug("name 	: "+multipartFile.getName());
+	            log.debug("filename : "+multipartFile.getOriginalFilename());
+	            log.debug("size 	: "+multipartFile.getSize());
+	            log.debug("-------------- file end --------------\n");
+	        }
+	    }*/
+	}
+
+	public Map<String, Object> selectBoardDetail(Map<String, Object> map) throws Exception {
+	    sampleDAO.updateHitCnt(map);
+	    Map<String, Object> resultMap = new HashMap<String,Object>();
+	    Map<String, Object> tempMap = sampleDAO.selectBoardDetail(map);
+	    resultMap.put("map", tempMap);
+	     
+	    List<Map<String,Object>> list = sampleDAO.selectFileList(map);
+	    resultMap.put("list", list);
+	    
+	    return resultMap;
+	}
+
+	@Override
+	public void updateBoard(Map<String, Object> map) throws Exception {
+		sampleDAO.updateBoard(map);
+	}
+
+	@Override
+	public void deleteBoard(Map<String, Object> map) throws Exception {
+		sampleDAO.updateBoard(map);
+	}
+	
+	
+	
+}
